76afdd7954c75e6c0037d292106fca9022688420
Marek Sagan <marek.sagan@gmail.com>
2020-01-11 17:03:54 +0100

    initial routing for hats

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div>
-      <HomePage />
+
+const HatsPage = () => (
+    <div>
+      <h1 class='title'>HATS PAGE</h1>
     </div>
-  
+  );
+
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+    </div>
+
+  )
 }
 
 export default App;
